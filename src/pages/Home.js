2dca14d418ac1a65c3d80c75e95a6ef454aa3c2b
Robert <79421897+rthebob@users.noreply.github.com>
2023-01-10 17:49:31 +0100

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
+import Exercises from '../components/Exercises';
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+
+const Home = () => {
+
+  const [bodyPart, setBodyPart] = useState('all')
+  const [exercises, setExercises] = useState([]);
+
+  console.log(bodyPart)
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises 
+      setExercises={setExercises}
+      bodyPart={bodyPart} 
+      setBodyPart={setBodyPart} 
+      />
+      <Exercises 
+      setExercises={setExercises}
+      bodyPart={bodyPart} 
+      exercises={exercises} 
+      />
+    </Box>
+  )
+}
+
+export default Home
