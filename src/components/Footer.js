1e61f9644f818a74f49157c3ba1b285df26c2fb9
Robert <79421897+rthebob@users.noreply.github.com>
2023-01-10 17:48:42 +0100

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#fff">
+    <Typography sx={{ fontSize: { lg: '15px', xs: '10px' } }} mt="41px" textAlign="center" pb="15px" fontFamily='Roboto'>rthebob @ 2023</Typography>
+  </Box>
+);
+
+export default Footer;
